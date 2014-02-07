3f6782045a55d27639cc7a5cc17661f4ad1987f7
hg222dt <hg222dt@student.lnu.se>
2014-02-07 21:02:12 +0100

    småfix på kontrollerna samt lagt grund till code behind

---
+++ SecretNumber/SecretNumber/default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SecretNumber
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //få ut ett slumpat hemligt tal
+        }
+
+        protected void GuessButton_Click(object sender, EventArgs e)
+        {
+            //Jämföra gissat tal med sparade tal
+            //Ev skriva ut lämpligt meddelande
+            //låsa fält och knappar om rätt gissning gjorts eller om gissningar är slut
+        }
+
+        protected void NewGameButton_Click(object sender, EventArgs e)
+        {
+            //få ut nytt slumpat hemligt tal.
+
+        }
+    }
+}
